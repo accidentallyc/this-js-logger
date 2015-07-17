de18e869108b1cee0a27c08b5b695af469ae7dcc
unknown <project.cassey@gmail.com>
2015-07-18 02:12:14 +0800

    Log standard exceptions to localStorage + autoclear at N records (default is 300 )

--- dist/this-js-logger.js
+++ dist/this-js-logger.js
@@ -1,6 +1,6 @@
 !function(){
 	//@TODO: Check for dependencies
-	var VERSION = "0.1";
+	var VERSION = "0.1.2";
 	
 	/** Get current script **/
 	var me = document.currentScript; //@TODO: Check for dependency
@@ -13,6 +13,9 @@
 		if( !logger_db ) logger_db = "thisJs_logger";
 	var logger_dbDescp = me.getAttribute("logger-db-descp");
 		if( !logger_dbDescp ) logger_dbDescp = "thisJs-logger logs table";
+	var logger_max_records = me.getAttribute("logger-max-records");
+		if( !logger_max_records ) logger_max_records = 300;
+		logger_remove_limit = logger_max_records * 0.40;
 	
 	var db = openDatabase( logger_db, '1.0', logger_dbDescp, 512);
 		db.transaction(function (tx) {
@@ -61,15 +64,19 @@
 			}
 			
 			if( isRecorded ){
-				db.transaction(function (tx) {
-					var logFileValue = type  + " : " + message + " -- " + now;
-						if( obj ) logFileValue += " " + JSON.stringify(obj) ;
-						logFileValue += "\r\n" + stack;
-					tx.executeSql('INSERT INTO '+logger_db+' (log) VALUES (?)',[logFileValue]);
-				});
+				logToWeb(type,message,stack,now,obj);
 			}//if logToWeb
 		}
 	}
+
+	function logToWeb(type,message,stack,time,obj){
+		db.transaction(function (tx) {
+		var logFileValue = type  + " : " + message + " -- " + time;
+			if( obj ) logFileValue += " " + JSON.stringify(obj) ;
+			logFileValue += "\r\n" + stack;
+		tx.executeSql('INSERT INTO '+logger_db+' (log) VALUES (?)',[logFileValue]);
+		});
+	}//logToWeb
 	window.log ={
 		error 	:factory("ERROR"),
 		warn 	:factory("WARN"),
@@ -81,7 +88,6 @@
 	}
 	
 	function download(){
-		
 		db.transaction(function (tx) {
 			tx.executeSql('SELECT log FROM ' + logger_db, [], function (tx, results) {
 				var contentArray = [];
@@ -97,8 +103,6 @@
 				a.click();
 			});
 		});	
-		
-       
 	}
 	
 	function clear(){
@@ -107,6 +111,47 @@
 			log.info("Cleared logs",null,false);
 		});	
 	}
+
 	
-	log.trace("RUNNING thisJs-logger v" +VERSION);
+	function winOnError(message,source,lineNum,d,errorObject){
+		var time = (new Date()).toLocaleString();
+		var type = "EXCEPTION";
+		var stack;
+		if( errorObject ){
+			stack = errorObject.stack;
+		} else{
+			stack = source + " @ line:"+ lineNum;
+		}
+		logToWeb(type,message,stack,time,errorObject);
+	}
+	if(  window.onerror ){
+		var winOnErrorTemp =  window.onerror;
+		window.onerror = function(){
+			winOnErrorTemp.apply(this,arguments);
+			winOnError.apply(this,arguments);
+		};
+	}
+	else
+		window.onerror = winOnError;
+
+	/** Some form of algo that checks #of logged "PAGES" in the db
+	to prevent file logs from getting too large **/
+	 db.transaction(function (tx) {
+		tx.executeSql('SELECT COUNT(*) AS c FROM ' + logger_db, [], function (tx, r) {
+			var count =  r.rows.item(0).c;
+			console.log(count);
+			if( parseInt(count) > logger_max_records ) {
+				
+				tx.executeSql('DELETE FROM '+logger_db+' WHERE rowid IN (SELECT rowid FROM '+logger_db+' ASC LIMIT '+logger_remove_limit+')',[],function(){
+					log.warn("Cleared 10 records from the db");
+				},function(tx,e){console.log(e);});
+			}
+		}, function (tx, e) {
+		  log.warn("Could not auto-clear: " + e.message);
+		});
+	  });
+
+	
+	log.trace("RUNNING thisJs-logger v" +VERSION + "--- ON PAGE: " + window.location );
+	//document.cookie.match( RegExp("happy=(.*?)(?:\;)") )
 }();
