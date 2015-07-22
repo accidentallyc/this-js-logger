dc49a34a2762e638643f30069393e088a8a2de74
Al Cassey Chavez <cassey.chavez@go-va.com.au>
2015-07-22 14:52:51 +0800

    new version 0.2.0, going forward with the logging categories

--- README.md
+++ README.md
@@ -17,19 +17,19 @@ log.clear();        //clear the WebSQL
 ## Sample Result
 ```
 WARN Warning, this version is out of date. -- 6/30/2015, 9:42:52 PM
-    at Object.warn (file:///F:/mygit/this-js-logger/dist/this-js-logger.js:50:17)
+    at Object.warn (/some_directory/dist/this-js-logger.js:50:17)
     at <anonymous>:2:5
     at Object.InjectedScript._evaluateOn (<anonymous>:895:140)
     at Object.InjectedScript._evaluateAndWrap (<anonymous>:828:34)
     at Object.InjectedScript.evaluate (<anonymous>:694:21)
 INFO Connect to XXX Server... -- 6/30/2015, 9:42:52 PM
-    at Object.info (file:///F:/mygit/this-js-logger/dist/this-js-logger.js:50:17)
+    at Object.info (/some_directory/dist/this-js-logger.js:50:17)
     at <anonymous>:3:5
     at Object.InjectedScript._evaluateOn (<anonymous>:895:140)
     at Object.InjectedScript._evaluateAndWrap (<anonymous>:828:34)
     at Object.InjectedScript.evaluate (<anonymous>:694:21)
 DEBUG Attempting to reconnect  -- 6/30/2015, 9:42:52 PM
-    at Object.debug (file:///F:/mygit/this-js-logger/dist/this-js-logger.js:50:17)
+    at Object.debug (/some_directory/dist/this-js-logger.js:50:17)
     at <anonymous>:4:5
     at Object.InjectedScript._evaluateOn (<anonymous>:895:140)
     at Object.InjectedScript._evaluateAndWrap (<anonymous>:828:34)
@@ -50,10 +50,57 @@ Below are the default configurations, They are all optional. You can use it as i
     logger-file-prefix = 'thisJs-logger'  /* when performing log.download() */
     logger-db = 'thisJs_logger'  /* name of the database and table where the logs are stored */
     logger-db-descp = ''  /* description of the db (might be used in the future for named logs */
+    logger-record-flag = 'false' /* If true, saves all the logs into the WebSQL */
 ></script>
 ```
 
 ## Caveats
 1. This only works with the later browser version (because HTML5). However i still have to pinpoint which browser versions it fails at.
 2. Since its using WebSQL it does have a table size limit. The default size is set to 512. Will work on a version where it implicity replace old records.
+
+
+## What's new?
+VERSION "0.2.0" has the ff new features
+1. thisJsLogger class ( allows you to instantiate your own logger )
+2. Logger categories ( allows you to categorize logging )
+3. `logger-record-flag` default changed from true to false.
+
+### Logger Categories
+Sample Result
+```
+< function binding >
+TRACE RUNNING thisJs-logger v0.2.0--- ON PAGE: /some_directory/sample.html -- 7/22/2015, 2:44:24 PM
+    at null.trace (/some_directory/dist/this-js-logger.js:88:17)
+    at new window.thisJsLogger (/some_directory/dist/this-js-logger.js:136:8)
+    at /some_directory/sample.html:76:16
+</ function binding >
+< function binding >
+INFO Finished binding function to the `the` word in <li> #1 -- 7/22/2015, 2:44:24 PM
+    at null.info (/some_directory/dist/this-js-logger.js:88:17)
+    at /some_directory/sample.html:83:7
+</ function binding >
+```
+
+Sample Html
+```html
+<script>
+    window.flog = new thisJsLogger({},"function binding");
+    flog.info("Finished binding function to the `the` word in <li> #1");
+</script>
+```
+
+If you are using logger categories, while debugging you can filter through the logs by
+appending #ns=cat1,cat2,cat3 or #ns=cat1.
+
+
+## FAQ
+What if i don't want to use `log` as the function name?
+
+```html
+<script src='dist/this-js-logger.js' logger-auto-instance = 'false'></script>
+<script>
+    yourOwnVariable = new thisJsLogger();
+    yourOwnVariable.log("you can now log via `yourOwnVariable` ");
+</script>
+```
 
--- dist/this-js-logger.js
+++ dist/this-js-logger.js
@@ -1,26 +1,68 @@
 !function(){
 	//@TODO: Check for dependencies
-	var VERSION = "0.1.2";
+	var VERSION = "0.2.0";
 	
 	/** Get current script **/
 	var me = document.currentScript; //@TODO: Check for dependency
+	var intLevels = {
+		ERROR:1,
+		WARN:2,
+		INFO:3,
+		DEBUG:4,
+		TRACE:5
+	};
+
+	function defaultify(objCollection){
+		for(var x in objCollection){
+			var tempAttr = me.getAttribute(x);
+			if( tempAttr ) {
+				switch(typeof(objCollection[x]) ){
+					case "number": objCollection[x] = parseInt(tempAttr); break;
+					case "boolean":  objCollection[x] = tempAttr == "true"; break;
+					default: objCollection[x] = tempAttr;
+				}
+			}
+		}
+		return objCollection;
+	}
+
+	var db;
+	function getDB(){
+		if( db ) return db;
+		else {
+			db = openDatabase( settings["logger-db"], '1.0', settings["logger-db-descp"], 512);
+			db.transaction(function (tx) {
+				tx.executeSql('CREATE TABLE IF NOT EXISTS '+settings["logger-db"]+' (log TEXT)');
+			});
+			return db;
+		}
+	}
 	
-	/** Settings p1 **/
-	var logger_prefix = me.getAttribute("logger-file-prefix");
-		if( !logger_prefix ) logger_prefix = "thisJs-logger";
-		logger_prefix +=  " - ";
-	var logger_db = me.getAttribute("logger-db");
-		if( !logger_db ) logger_db = "thisJs_logger";
-	var logger_dbDescp = me.getAttribute("logger-db-descp");
-		if( !logger_dbDescp ) logger_dbDescp = "thisJs-logger logs table";
-	var logger_max_records = me.getAttribute("logger-max-records");
-		if( !logger_max_records ) logger_max_records = 300;
-		logger_remove_limit = logger_max_records * 0.40;
+	/** Settings **/
+	var temp = Object.create(null,{});
+		temp["logger-prefix"]		= "thisJs-logger",
+		temp["logger-db"]			= "choose_a_unique_name_for_your_website",
+		temp["logger-db-descp"]		= "thisJs-logger logs table",
+		temp["logger-max-records"]	= 300,
+		temp["logger-auto-instance"]= true,
+		temp["logger-level"]		= "TRACE",
+		temp["logger-record-flag"]	= false
+	var settings = defaultify(temp);
+
+	settings["logger-prefix"] +=  " - ";
+	settings["logger-remove-limit"] = settings["logger-max-records"] * 0.40;
 	
-	var db = openDatabase( logger_db, '1.0', logger_dbDescp, 512);
-		db.transaction(function (tx) {
-			tx.executeSql('CREATE TABLE IF NOT EXISTS '+logger_db+' (log TEXT)');
-		});
+	var hash = window.location.hash;
+	var debug_settings = Object.create(null,{});
+		debug_settings.ns = null;
+		debug_settings.ns_list = null;
+	var reg = /([0-9a-zA-Z_-]*)=([0-9a-zA-Z_-\s]*)/g;
+	var match = reg.exec(hash);;
+	while (match != null){
+	    debug_settings[match[1]] = (2 in match) ? match[2] : true;
+	    match = reg.exec(hash);
+	}
+	if(debug_settings.ns) debug_settings.ns_list = debug_settings.ns.split(",");
 	
 	var style = {
 		ERROR: { header: "font-weight:bold;color:red;", body: "font-weight:normal;color:red;" },
@@ -29,28 +71,24 @@
 		DEBUG : { header: "font-weight:bold;color:#01014C;", body: "font-weight:normal;color:#01014C;" },
 		TRACE : { header: "font-weight:bold;color:#054C05;", body: "font-weight:normal;color:#054C05;" }
 	}
-	
-	var intLevels = {
-		ERROR:1,
-		WARN:2,
-		INFO:3,
-		DEBUG:4,
-		TRACE:5
-	};
-	
-	/** Settings p2 **/
-	var logLevel = me.getAttribute("logger-level");
-		if( logLevel == undefined ) logLevel = intLevels[ "TRACE" ];
-		else logLevel = intLevels[ logLevel.toUpperCase() ];
-
-	function factory(type){
+	var blank_func = function(){};
+	window.debug_settings = debug_settings;
+	function factory(that,type,namespace){
 		var s = style[type];
-		return function(message,obj,isRecorded){
-			if( logLevel < intLevels[type] ) return;
+		var isRecorded = that.settings["logger-record-flag"];
+		var isNotLeveled = intLevels[that.settings["logger-level"].toUpperCase()] < intLevels[type];
+		var isNotInDebugCategory = debug_settings.ns_list === null ? false : (debug_settings.ns_list.indexOf(namespace) == -1) ;
+
+		if( isNotLeveled || isNotInDebugCategory ) return blank_func;
+		return function(message,obj){
+			
 		
 			if( isRecorded == undefined ) isRecorded = true;
 			var now = (new Date()).toLocaleString();
 			var stack = (new Error()).stack.substring(6);
+
+			var openNS = namespace ? "< " + namespace + " >\n":"";
+			var closeNS = namespace ? "\n</ " + namespace + " >":"";
 			
 
 			if( obj ){
@@ -59,7 +97,7 @@
 				console.log( "%c" + stack, s.body );
 			}
 			else{
-				var stackMessage =  "%c"+ type  +" %c"+message + " -- " + now + "\n"+stack
+				var stackMessage =  openNS + "%c"+ type  +" %c"+message + " -- " + now + "\n"+stack + closeNS
 				console.log( stackMessage, s.header, s.body );
 			}
 			
@@ -70,26 +108,41 @@
 	}
 
 	function logToWeb(type,message,stack,time,obj){
+		var db = getDB();
 		db.transaction(function (tx) {
 		var logFileValue = type  + " : " + message + " -- " + time;
 			if( obj ) logFileValue += " " + JSON.stringify(obj) ;
 			logFileValue += "\r\n" + stack;
-		tx.executeSql('INSERT INTO '+logger_db+' (log) VALUES (?)',[logFileValue]);
+		tx.executeSql('INSERT INTO '+settings["logger-db"]+' (log) VALUES (?)',[logFileValue]);
 		});
 	}//logToWeb
-	window.log ={
-		error 	:factory("ERROR"),
-		warn 	:factory("WARN"),
-		info 	:factory("INFO"),
-		debug 	:factory("DEBUG"),
-		trace	:factory("TRACE"),
-		download:download,
-		clear	:clear
-	}
+
+	window.thisJsLogger = function(newSettings,name){
+		name = (name ? name : "");
+		this.settings = Object.create(null,{});
+		for(var x in settings){
+			if( newSettings && x in newSettings) this.settings[x] = newSettings[x];
+			else this.settings[x] = settings[x];
+		}
+
+		this.error 	= factory(this,"ERROR", name );
+		this.warn 	= factory(this,"ERROR", name);
+		this.info 	= factory(this,"INFO",name);
+		this.debug 	= factory(this,"DEBUG",name);
+		this.trace 	= factory(this,"TRACE",name);
+		this.clear 	= clear;
+		this.download = download;
+
+		this.trace("RUNNING thisJs-logger v" +VERSION + "--- ON PAGE: " + window.location );
+	};
+
+	if( settings["logger-auto-instance"] )	window.log = new thisJsLogger();
+
+
 	
 	function download(){
 		db.transaction(function (tx) {
-			tx.executeSql('SELECT log FROM ' + logger_db, [], function (tx, results) {
+			tx.executeSql('SELECT log FROM ' + settings["logger-db"], [], function (tx, results) {
 				var contentArray = [];
 				var len = results.rows.length, i;
 				for (i = 0; i < len; i++) {
@@ -99,7 +152,7 @@
 				var a = document.createElement('a');
 				var blob = new Blob( contentArray, {'type':'application/octet-stream'});
 				a.href = window.URL.createObjectURL(blob);
-				a.download = logger_prefix + (new Date()).toUTCString() + ".txt";
+				a.download = settings.logger_prefix + (new Date()).toUTCString() + ".txt";
 				a.click();
 			});
 		});	
@@ -107,7 +160,7 @@
 	
 	function clear(){
 		db.transaction(function (tx) {
-			tx.executeSql('DELETE FROM ' + logger_db );
+			tx.executeSql('DELETE FROM ' + settings["logger-db"] );
 			log.info("Cleared logs",null,false);
 		});	
 	}
@@ -124,6 +177,7 @@
 		}
 		logToWeb(type,message,stack,time,errorObject);
 	}
+
 	if(  window.onerror ){
 		var winOnErrorTemp =  window.onerror;
 		window.onerror = function(){
@@ -134,23 +188,29 @@
 	else
 		window.onerror = winOnError;
 
-	/** Some form of algo that checks #of logged "PAGES" in the db
-	to prevent file logs from getting too large **/
-	 db.transaction(function (tx) {
-		tx.executeSql('SELECT COUNT(*) AS c FROM ' + logger_db, [], function (tx, r) {
+	/** 
+	* Some form of algo that checks #of logged "PAGES" in the db
+	* to prevent file logs from getting too large. Written inside
+	* an interval to insure that the db has been made.
+	*
+	*/
+	window.setInterval(function(){
+		var db = getDB();
+		 db.transaction(function (tx) {
+		tx.executeSql('SELECT COUNT(*) AS c FROM ' + settings["logger-db"], [], function (tx, r) {
 			var count =  r.rows.item(0).c;
-			if( parseInt(count) > logger_max_records ) {
+			if( parseInt(count) > settings["logger-max-records"] ) {
 				
-				tx.executeSql('DELETE FROM '+logger_db+' WHERE rowid IN (SELECT rowid FROM '+logger_db+' ASC LIMIT '+logger_remove_limit+')',[],function(){
-					log.warn("Cleared 10 records from the db");
+				tx.executeSql('DELETE FROM '+settings["logger-db"]+' WHERE rowid IN (SELECT rowid FROM '+settings["logger-db"]+' ASC LIMIT '+ settings["logger-remove-limit"] +')',[],function(){
+					log.warn("Cleared "+settings["logger-remove-limit"]+" records from the db");
 				},function(tx,e){console.log(e);});
 			}
 		}, function (tx, e) {
 		  log.warn("Could not auto-clear: " + e.message);
 		});
 	  });
-
+	},200);
 	
-	log.trace("RUNNING thisJs-logger v" +VERSION + "--- ON PAGE: " + window.location );
+
 	//document.cookie.match( RegExp("happy=(.*?)(?:\;)") )
 }();
